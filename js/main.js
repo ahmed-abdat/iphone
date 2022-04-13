6aa06e7a3440d735647d7e4073e772658849971f
ahmed-abdat <ahmedeabdat@gmail.com>
2022-04-13 22:46:41 +0000

    news updates

--- js/main.js
+++ js/main.js
@@ -11,3 +11,15 @@ function phones(phone) {
 function colors(color) {
     container.style.background = color;
 }
+
+let nav = document.querySelector('.nav');
+
+nav.onclick = function() {
+    nav.classList.toggle('active')
+}
+
+let body = document.querySelector('body');
+
+nav.addEventListener('click',function(){
+    body.classList.toggle('open');
+})
